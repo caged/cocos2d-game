3a7dd618f79b72da6d92cb3bbce70caa3c277433
Justin Palmer <justin@activereload.net>
2009-02-03 10:45:14 -0800

    Initial import

---
+++ Classes/___PROJECTNAMEASIDENTIFIER___AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  ___PROJECTNAMEASIDENTIFIER___AppDelegate.m
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import "___PROJECTNAMEASIDENTIFIER___AppDelegate.h"
+
+
+@implementation ___PROJECTNAMEASIDENTIFIER___AppDelegate
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+	
+}
+
+@end
